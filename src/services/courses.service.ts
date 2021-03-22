3b65e4b5239c850f2a3bcc499ebdd5675791d2d9
Martin Grellmann <martin.grellmann@sengicon.de>
2021-03-22 20:51:34 +0100

    7. Consuming Observable-based services using the Angular async Pipe

---
+++ src/services/courses.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Course} from '../app/model/course';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+  })
+
+/**
+ * stateless observable Service
+ */
+export class CoursesService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>('/api/courses')
+      .pipe(
+        map(res => res['payload'])
+      );
+  }
+}

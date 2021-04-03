28a76d345dd80090706f42088d6654cda0282928
Martin Grellmann <martin.grellmann@sengicon.de>
2021-04-03 12:55:31 +0200

    16. Understanding the Angular Component providers property

--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -10,6 +10,10 @@ export class LoadingService {
 
   loading$: Observable<boolean> = this.loadingSubject.asObservable();
 
+  constructor() {
+    console.log('Loading service created ...');
+  }
+
   showLoaderUntilCompleted<T>(obs$: Observable<T>): Observable<T> {
     return of(null)
       .pipe(

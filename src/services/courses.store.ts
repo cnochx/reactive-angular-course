f03f183e689b97cbec6c75c5e3dce470b1b508ea
Martin Grellmann <martin.grellmann@sengicon.de>
2021-05-16 22:21:20 +0200

    23. Step-by-Step Implementation of an Angular Store Service

--- src/services/courses.store.ts
+++ src/services/courses.store.ts
@@ -1,22 +1,54 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable, throwError} from 'rxjs';
 import {Course, sortCoursesBySeqNo} from '../app/model/course';
+import {HttpClient} from '@angular/common/http';
+import {LoadingService} from '../app/loading/loading.service';
+import {MessagesService} from '../app/messages/messages.service';
+import {catchError, map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesStore {
 
-  courses$: Observable<Course[]>;
+  private subject = new BehaviorSubject<Course[]>([]);
+
+  courses$: Observable<Course[]> = this.subject.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private loading: LoadingService,
+    private messages: MessagesService) {
+
+    this.loadAllCourses();
+  }
+
+  private loadAllCourses() {
+
+    const loadCourses$ = this.http.get<Course[]>('/api/courses')
+      .pipe(
+        map(response => response['payload']),
+        catchError(err => {
+          const message = 'Could not load courses';
+          this.messages.showErrors(message);
+          console.log(message, err);
+          return throwError(err);
+        }),
+        tap(courses => this.subject.next(courses))
+      );
+
+    this.loading.showLoaderUntilCompleted(loadCourses$)
+      .subscribe();
+  }
 
   filterByCategory(category: string): Observable<Course[]> {
     return this.courses$
       .pipe(
         map(courses =>
-          courses.filter(courses => course.category === category)
+          courses.filter(course => course.category === category)
             .sort(sortCoursesBySeqNo)
         )
       )
   }
-m
+
 }
